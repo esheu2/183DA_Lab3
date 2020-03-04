29aeaca81016e8932bd0d4fdf883ea7618d4fa02
Everett Sheu <esheu2@gmail.com>
2020-03-04 12:34:22 -0800

    stuff

--- comms.py
+++ comms.py
@@ -1,36 +1,24 @@
-import time, mscvcrt
 import socket
 
-host = 192.168.4.1
-port = 80
+'''
+pip install py-getch
+'''
 
-keypressTimeout = 3
+host = "192.168.4.1"
+port = 80
 
 cmd = None
 prev_cmd = 'TO'
 out = None
 while True:
-    timeout = time.time()
-    if mscvrt.kbhit():
-        prev_cmd = cmd
-        cmd = msvcrt.getch().decode()
-    elif time.time() - timeout > keypressTimeout:
-        prev_cmd = cmd
-        cmd = 'TO'
-
-    # WASD controls
-    if cmd == 'w':
-        out = 'F'
-    elif cmd == 'a':
-        out = 'L'
-    elif cmd == 's':
-        out = 'B'
-    elif cmd == 'd':
-        out = 'R'
+    cmd = 'F'
+    # cmd can be F, B, R, L
 
     # connect to paperbot websocket
     sock = socket.socket()
     sock.connect((host, port))
-    sock.send(out.encode())
+    sock.send(cmd.encode())
     # data = sock.recv(1) is used to ask paperbot for data
     # NOTE: paperbot side of the code has not yet been implemented
+    # Everything was implemented using the following information
+    # https://techtutorialsx.com/2017/11/13/esp32-arduino-setting-a-socket-server/
